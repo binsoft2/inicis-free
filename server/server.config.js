3a0da959d580fa4433dbf4e4a62555bb021d3444
JeongSooHHH <space2577@gmail.com>
2023-02-10 17:41:54 +0900

    ADD: 결제모듈(서버)

---
+++ server/server.config.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'inicis free',
+    allowedOrigin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    cookie: {
+        maxAge: 1209600000,
+        httpOnly: true,
+    },
+    db: {},
+};
